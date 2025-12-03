db5f563385012e59fc673c3372f1e18e60da8d5f
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-12-03 11:37:40 +0100

    css de la liste faite

--- pokemon-react/src/components/Pokemon.jsx
+++ pokemon-react/src/components/Pokemon.jsx
@@ -6,9 +6,9 @@ export default function Pokemon({pokemons}) {
   return (
     <div className="pokemon">
       <div key={pokemons.id} className="pokemon-card">
+        <p>{pokemons.pokedexId}</p>
         <h3>{pokemons.name}</h3>
-        <img src={pokemons.image} alt={pokemons.name} />
-        <p>ID : {pokemons.pokedexId}</p>
+        <img className="pokemon-image"src={pokemons.image} alt={pokemons.name} />
       </div>
     </div>
   );
