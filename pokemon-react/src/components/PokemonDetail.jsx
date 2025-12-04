71f94bbc8f26d124e2a33af98735a877c7938e75
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-12-04 10:53:31 +0100

    types ajoutés

--- pokemon-react/src/components/PokemonDetail.jsx
+++ pokemon-react/src/components/PokemonDetail.jsx
@@ -1,34 +1,39 @@
 import Pokemon from "./Pokemon"
 import { useEffect, useState } from 'react'
 
-export default function PokemonDetail({currentPokemon}) {
+export default function PokemonDetail({ currentPokemon }) {
 
   return (
-   <>
+    <>
 
-     
+
       <div className="pokemon-detail"  >
-                
-  
-              
-                 <Pokemon pokemons={currentPokemon}/>
-  
-               <div className="types-container">
-                <h4 className="title-types">Types</h4>
-                <div className="types-container-images">
-                  <img src={currentPokemon.apiTypes.image} alt="" />
-                  <img src={currentPokemon.apiTypes.image} alt="" />
-                </div>
-               </div>
 
-               <div className="container-evolution">
-                   <Pokemon pokemons={currentPokemon}/>
-                   {/* //mettre l'évolution ici àla place de currentpokemeonjepense */}
-               </div>
+
+
+        <Pokemon pokemons={currentPokemon} />
+
+        <div className="types-container">
+          <h4 className="title-types">Types</h4>
+          <div className="types-container-images">
+            {currentPokemon.apiTypes.map(type =>
+
+                (  <img key={type.name}src={type.image} alt={type.name} />
+                )
+            )
+
+            }
+          </div>
+        </div>
+
+        <div className="container-evolution">
+          <Pokemon pokemons={currentPokemon} />
+          {/* //mettre l'évolution ici àla place de currentpokemeonjepense */}
+        </div>
       </div>
-   
-   
-   </>
+
+
+    </>
   );
 
 
