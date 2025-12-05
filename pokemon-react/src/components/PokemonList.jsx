98770f453633fb31701920ace70cc8a4ed290f25
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-12-05 11:45:07 +0100

    burger 45% fait

--- pokemon-react/src/components/PokemonList.jsx
+++ pokemon-react/src/components/PokemonList.jsx
@@ -3,11 +3,34 @@ import { useEffect, useState } from 'react'
 
 export default function PokemonList({ pokemons, setPokemon }) {
 
+function myFunction() {
+  const hiddenlist = document.getElementById("pokemon-list-mobile");
+  if (hiddenlist.style.display === "block") {
+    hiddenlist.style.display = "none";
+  } else {
+    hiddenlist.style.display = "block";
+  }
+}
+
+
+
 
   return (
     <>
 
-      <div className="pokemon-list">
+      <div class="topnav">
+        <a href="#home" class="active">Logo</a>
+        <div id="pokemon-list-mobile">
+          <a href="#news">News</a>
+          <a href="#contact">Contact</a>
+          <a href="#about">About</a>
+        </div>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+          ☰</a>
+      </div>
+
+
+      <div className="pokemon-list" >
         {pokemons.map(poke => (
           <div key={poke.id} onClick={() => setPokemon(poke)}>
             <Pokemon pokemons={poke} />
@@ -15,6 +38,8 @@ export default function PokemonList({ pokemons, setPokemon }) {
 
         ))}
       </div>
+
+
 
 
     </>
